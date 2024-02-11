52de64bf73d0474782e310330a922498a272d75e
Jair Torezone <jairtorezone@gmail.com>
2024-02-11 10:14:49 -0300

    select

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,22 @@
 const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError("Nome do usuário é obrigatório!");
+    const database = await sqliteConnection();
+
+    const checkUsersEmailExist = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUsersEmailExist) {
+      throw new AppError("Email já esta cadastrado");
     }
 
-    return response.status(201).json({ name, email, password });
+    return response.status(201).json();
   }
 }
 
