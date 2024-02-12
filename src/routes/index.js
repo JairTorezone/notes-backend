66725a44ea8ca3b8461d03e18121a20ae140771b
Jair Torezone <jairtorezone@gmail.com>
2024-02-11 22:07:12 -0300

    Getting note tags and listing the tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 
 const userRoutes = require("./users.routes");
 const noteRoutes = require("./notes.routes");
+const tagsRoutes = require("./tags.routes");
 
 const routes = Router();
 routes.use("/users", userRoutes);
 routes.use("/notes", noteRoutes);
+routes.use("/tags", tagsRoutes);
 
 module.exports = routes;
